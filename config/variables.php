4de2e27b88916bd54c429dd4bc5a33145e197279
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2023-06-26 16:50:40 +0700

    :sparkles: (feat): change link socmed

--- config/variables.php
+++ config/variables.php
@@ -7,8 +7,8 @@ return [
     "templateSuffix" => "Moch Azmi Iskandar",
     "templateVersion" => "1.0.0",
     "templateFree" => false,
-    "templateDescription" => "Start your development with a Dashboard for Bootstrap 5",
-    "templateKeyword" => "dashboard, bootstrap 5 dashboard, bootstrap 5 design, bootstrap 5",
+    "templateDescription" => "Begin your journey with Megalos, a comprehensive Hotspot Management System powered by Varnion.com. Streamline your operations with our user-friendly interface and robust features.",
+    "templateKeyword" => "hotspot management system, Megalos, Varnion, Wi-Fi management, network management, hotspot control, Varnion Megalos, reliable hotspot system",
     "licenseUrl" => "https://themeforest.net/licenses/standard",
     "livePreview" => "https://pixinvent.com/demo/vuexy-html-bootstrap-admin-template/landing-page/",
     "productPage" => "https://1.envato.market/vuexy_admin",
@@ -18,12 +18,8 @@ return [
     "generator" => "",
     "changelog" => "https://pixinvent.com/demo/vuexy-html-bootstrap-admin-template/vuexy-changelog.html",
     "repository" => "https://github.com/pixinvent/vuexy-html-admin-template",
-    "gitRepo" => "pixinvent",
-    "gitRepoAccess" => "vuexy-html-admin-template",
-    "githubFreeUrl" => "https://pixinvent.com/tools/github/github-access",
-    "facebookUrl" => "https://www.facebook.com/pixinvents/",
-    "twitterUrl" => "https://twitter.com/pixinvents",
-    "githubUrl" => "https://github.com/pixinvent",
-    "dribbbleUrl" => "https://dribbble.com/pixinvent",
-    "instagramUrl" => "https://www.instagram.com/pixinvents/"
+    "gitRepo" => "api-mikrotik-megalos",
+    "gitRepoAccess" => "laravel-vuexy-api-mikrotik-megalos",
+    "linkedInUrl" => "https://www.linkedin.com/company/pt-varnion-technology-semesta/",
+    "instagramUrl" => "https://www.instagram.com/varnionsemesta/"
 ];
