f9cd7a243e2eb525960d3aaa1ddc9100a894a8af
Ryuinferno <ryuinferno@rizulhanif.com>
2023-06-30 09:41:49 +0700

    auto detect server protocol
    new file:   app/Helpers/ProtocolCheck.php
    modified:   app/Providers/AppServiceProvider.php

---
+++ app/Helpers/ProtocolCheck.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Helpers;
+
+class ProtocolCheck
+{
+    public static function check()
+    {
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+            $_SERVER['HTTPS'] = 'on';
+        }
+        if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
+            $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
+        }
+
+        return (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? true : false;
+    }
+}
