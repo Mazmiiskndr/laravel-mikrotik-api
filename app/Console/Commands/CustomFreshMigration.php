53bed76e13868f3f9b90fa2e56b977edcf049def
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2023-06-30 12:44:15 +0700

    :sparkles: (feat):create custom migration & seeder

---
+++ app/Console/Commands/CustomFreshMigration.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CustomFreshMigration extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'migrate:customfresh';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Custom fresh migration that ignores specific tables';
+
+    /**
+     * List of tables to ignore.
+     * Add your FreeRADIUS tables here.
+     *
+     * @var array
+     */
+    protected $ignoreTables = [
+        'radacct',
+        'radcheck',
+        'radgroupcheck',
+        'radgroupreply',
+        'radreply',
+        'radusergroup',
+        'nas'
+        // add more tables here if needed...
+    ];
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // Get the list of all table names.
+        $tableNames = DB::connection()->getDoctrineSchemaManager()->listTableNames();
+
+        // Filter out the ignored tables.
+        $tablesToDrop = array_diff($tableNames, $this->ignoreTables);
+
+        // Drop all tables except the ignored ones.
+        foreach ($tablesToDrop as $tableName) {
+            Schema::dropIfExists($tableName);
+        }
+
+        // Run the migrations.
+        $this->call('migrate', ['--force' => true]);
+
+        // Run the seeders.
+        $this->call('db:seed', ['--force' => true]);
+    }
+}
--- database/migrations/2023_03_31_065501_create_radcheck_table.php
+++ database/migrations/2023_03_31_065501_create_radcheck_table.php
@@ -14,15 +14,17 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('radcheck', function (Blueprint $table) {
-            $table->id();
-            $table->string('username', 64);
-            $table->string('attribute', 64);
-            $table->string('op', 2)->default("==");
-            $table->string('value', 253);
-        });
-        // Add index with specific length
-        DB::statement('CREATE INDEX username_index ON radcheck (username(32));');
+        if (!Schema::hasTable('radcheck')) {
+            Schema::create('radcheck', function (Blueprint $table) {
+                $table->id();
+                $table->string('username', 64);
+                $table->string('attribute', 64);
+                $table->string('op', 2)->default("==");
+                $table->string('value', 253);
+            });
+            // Add index with specific length
+            DB::statement('CREATE INDEX username_index ON radcheck (username(32));');
+        }
     }
 
     /**
