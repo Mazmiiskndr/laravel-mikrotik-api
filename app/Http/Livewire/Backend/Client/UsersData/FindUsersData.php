7000fcf91be4ac6b63df335222b8765c54d29147
Moch Azmi Iskandar <azmiiskandar0@gmail.com>
2023-07-07 14:02:00 +0700

    🔧 fix(users-data): enhance date range validation in FindUsersData component
    - Improve validation rules for 'fromDate' and 'toDate' fields
    - Add error handling for 'fromDate' when it is not set
    - Ensure that 'toDate' is always after or equal to 'fromDate'
    
    This commit improves the user experience by providing more accurate error messages and ensures that the date range input by the user is valid.

--- app/Http/Livewire/Backend/Client/UsersData/FindUsersData.php
+++ app/Http/Livewire/Backend/Client/UsersData/FindUsersData.php
@@ -26,22 +26,28 @@ class FindUsersData extends Component
     {
         // Here we validate the input for fromDate and toDate fields.
         $this->validateOnly($field, [
-            'fromDate' => 'date_format:Y-m-d|nullable',
-            'toDate' => 'date_format:Y-m-d|nullable',
+            'fromDate' => 'required|date_format:Y-m-d',
+            'toDate' => 'required_with:fromDate|date_format:Y-m-d|after_or_equal:fromDate',
+        ], [
+            'fromDate.required' => 'The From Date must be filled first.',
+            'toDate.required_with' => 'The To Date must be filled when From Date is present.',
+            'toDate.after_or_equal' => 'The To Date must be after or equal to From Date.',
         ]);
 
         // Check if the fromDate is later than the toDate. If so, add an error message to the 'toDate' field.
         if ($this->fromDate > $this->toDate) {
-            $this->addError('toDate', 'The To Date must be after From Date.');
+            $this->addError('toDate', 'The To Date must be after or equal to From Date.');
         } else {
-            $data = [
-                'fromDate' => $this->fromDate,
-                'toDate' => $this->toDate,
-            ];
-            // If the dates are valid, we emit an event called 'dateUpdated'.
-            $this->emit('dateUpdated', $data);
+            if (empty($this->fromDate)) {
+                $this->addError('fromDate', 'The From Date must be filled first.');
+            } else {
+                $data = [
+                    'fromDate' => $this->fromDate,
+                    'toDate' => $this->toDate,
+                ];
+                // If the dates are valid, we emit an event called 'dateUpdated'.
+                $this->emit('dateUpdated', $data);
+            }
         }
-
     }
-
 }
